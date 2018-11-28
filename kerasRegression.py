b39483d1b9812c191fcc9100e46bf84d75f85632
James Godwin <james.a.w.godwin@gmail.com>
2018-11-27 18:50:12 -0800

    keras regression model with k-fold cross validation

---
+++ kerasRegression.py
@@ -0,0 +1,44 @@
+# -*- coding: utf-8 -*-
+
+import numpy as np
+import pandas as pd
+from keras.models import Sequential
+from keras.layers import Dense
+from keras.layers import Dropout
+from keras.wrappers.scikit_learn import KerasRegressor
+from sklearn.model_selection import cross_val_score
+from sklearn.model_selection import KFold
+from sklearn.preprocessing import StandardScaler
+from sklearn.pipeline import Pipeline
+
+# load dataset
+dataframe = pd.read_csv("housing.csv", delim_whitespace=True, header=None)
+dataset = dataframe.values
+# split into input (X) and output (Y) variables
+X = dataset[:,0:13]
+y = dataset[:,13]
+
+
+# define base model
+def baseline_model():
+	# create model
+	model = Sequential()
+	model.add(Dense(13, input_dim=13, kernel_initializer='normal', activation='relu'))
+	model.add(Dense(1, kernel_initializer='normal'))
+	# Compile model
+	model.compile(loss='mean_squared_error', optimizer='adam')
+	return model
+ 
+#fix random seed for reproducibility   
+seed = 7
+np.seed(seed)
+# evaluate model with standardized dataset
+estimator = KerasRegressor(build_fn=baseline_model, epochs=100, batch_size=5, verbose=0)
+
+
+
+
+
+
+
+
