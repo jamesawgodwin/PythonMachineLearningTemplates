c4c29c77d8259fe3de20353ec1790a65a695de3b
James <james.a.w.godwin@gmail.com>
2019-02-22 15:10:46 -0800

    Made pycodestyle - formerly pep8 - changes

--- Random_walk_game.py
+++ Random_walk_game.py
@@ -1,28 +1,29 @@
 # -*- coding: utf-8 -*-
 """
-game that determines probability that you will reach height of 60 steps given steps 
-taken by roll of dice and clumsinesss. Start at 0 height and roll dice 100 times,
-simulate 10000 times. Display results. Calculate probability of reaching height of 60 steps
+game that determines probability that you will reach height of 60 steps given
+steps taken by roll of dice and clumsinesss. Start at 0 height and roll dice
+100 times, simulate 10000 times. Display results. Calculate probability of
+reaching height of 60 steps
 """
 
 import matplotlib.pyplot as plt
 import numpy as np
-#set seed
+# set seed
 np.random.seed(123)
 all_walks = []
-#number of interations
+# number of interations
 for i in range(500) :
     random_walk = [0]
-    #roll dice range() times per iteration
+    # roll dice range() times per iteration
     for x in range(100) :
         step = random_walk[-1]
-        dice = np.random.randint(1,7)
+        dice = np.random.randint(1, 7)
         if dice <= 2:
             step = max(0, step - 1)
         elif dice <= 5:
             step += 1
         else:
-            step = step + np.random.randint(1,7)
+            step = step + np.random.randint(1, 7)
         if np.random.rand() <= 0.001 :
             step = 0
         random_walk.append(step)
@@ -42,6 +43,6 @@ plt.ylabel("Number of times Height reached")
 plt.show()
 plt.clf()
 
-#winning percentage
-wp = (sum(ends >= 60)/ len(ends)) * 100
+# winning percentage
+wp = (sum(ends >= 60) / len(ends)) * 100
 print(wp)
