4c61dbd358fb491a0d15bb14561505ce3425c70b
James <james.a.w.godwin@gmail.com>
2019-02-22 15:19:40 -0800

    pycodestyle changes to script

--- Modular_Data_Validation_Script.py
+++ Modular_Data_Validation_Script.py
@@ -1,32 +1,36 @@
-#pip install cerberus (add !pip for notebook issues)
-#pip install python-magic-win64
+# pip install cerberus (add !pip for notebook issues)
+# pip install python-magic-win64
+# import cerberus //json specific
 from winmagic import magic
 import glob
-#import cerberus //json specific
 import os
 import re
 import dateutil.parser as dparser
+
 
 def check_file_type(file):
     """check for file types in directory"""
     file_type = magic.from_file(file)
     print(f"File type: {file_type}")
-    
+
+
 def check_missing_data(file):
     """check for missing relevant data"""
     if bool(re.search('cheese|ham|blah', open(file).read().lower())):
         print(f"Relevant data: \t Present")
-    else: 
+    else:
         print(f"Relevant data: \t Missing")
-        
+
+
 def get_upload_date(file):
     """Find and parse the date uploaded"""
     with open(file, "r") as f:
         for line in f.readlines():
             if line.startswith("Cheese is good"):
-                date = dparser.parse(line,fuzzy=True).date()
+                date = dparser.parse(line, fuzzy=True).date()
                 print(f"Date uploaded: \t {date}")
-                
+
+
 def check_relevant_data(directory):
     """function to check the file type and presence of search term"""
     
@@ -38,4 +42,4 @@ def check_relevant_data(directory):
         get_upload_date(file)
         
         print("\n")
-#check_relevant_data(full path here)
+# check_relevant_data(full path here)
