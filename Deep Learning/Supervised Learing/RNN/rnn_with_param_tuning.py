3edbc3b0cf1b009f5e686fa1723e7c579edbbebb
James <james.a.w.godwin@gmail.com>
2019-03-29 10:57:38 -0700

    added parameter tuning

--- Deep Learning/Supervised Learing/RNN/rnn_with_param_tuning.py
+++ Deep Learning/Supervised Learing/RNN/rnn_with_param_tuning.py
@@ -101,3 +101,29 @@ plt.show()
 import math
 from sklearn.metrics import mean_squared_error
 rmse = math.sqrt(mean_squared_error(real_stock_price, predicted_stock_price))
+
+"""
+# Tuning the RNN
+from keras.wrappers.scikit_learn import KerasClassifier
+from sklearn.model_selection import GridSearchCV
+from keras.models import Sequential
+from keras.layers import Dense
+def build_classifier(optimizer):
+    classifier = Sequential()
+    classifier.add(Dense(units = 50, kernel_initializer = 'uniform', activation = 'relu', input_dim = 11))
+    classifier.add(Dense(units = 50, kernel_initializer = 'uniform', activation = 'relu'))
+    classifier.add(Dense(units = 1, kernel_initializer = 'uniform', activation = 'sigmoid'))
+    classifier.compile(optimizer = optimizer, loss = 'binary_crossentropy', metrics = ['accuracy'])
+    return classifier
+classifier = KerasClassifier(build_fn = build_classifier)
+parameters = {'batch_size': [25, 32],
+              'epochs': [100, 5],
+              'optimizer': ['adam', 'rmsprop']}
+grid_search = GridSearchCV(estimator = classifier,
+                           param_grid = parameters,
+                           scoring = 'neg_mean_squared_error',
+                           cv = 10)
+grid_search = grid_search.fit(X_train, y_train)
+best_parameters = grid_search.best_params_
+best_accuracy = grid_search.best_score_
+"""
